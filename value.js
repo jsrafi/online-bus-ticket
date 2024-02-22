3dd2f612a45a9d71b57be7d15d629fb21d7f343f
jsrafi <js.rafi999@gmail.com>
2024-02-22 23:35:16 +0600

    first commit

---
+++ value.js
@@ -0,0 +1,346 @@
+function text()
+{
+  const btn1= document.getElementById('btn1');
+  btn1.style.backgroundColor='greenYellow'
+  const newValue = document.getElementById('set-value');
+  const li = document.createElement('section')
+  li.innerHTML=
+  " <div class='flex gap-16 px-5'><p>A1</p> <p> Economy </p> <p>550</p> ";
+  newValue.appendChild(li);
+
+  const seatLeft = document.getElementById('seat-left')
+  const seatLeftV = parseInt(seatLeft.innerText);
+  seatLeft.innerText = seatLeftV - 1;
+
+  const finalValue = document.getElementById('final-man')
+  const finalValueV= parseInt(finalValue.innerText);
+  finalValue.innerText = finalValueV + 1;
+  
+    
+  
+}
+function text2()
+{
+    const btn2= document.getElementById('btn2');
+    btn2.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>A2</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+}
+  
+ 
+
+function text3()
+{
+    const btn3= document.getElementById('btn3');
+    btn3.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>B1</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+}
+
+function text4()
+{
+    const btn4= document.getElementById('btn4');
+    btn4.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>B2</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+
+function text5()
+{
+    const btn5= document.getElementById('btn5');
+    btn5.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>C1</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+
+function text6()
+{
+    const btn6= document.getElementById('btn6');
+    btn6.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>C2</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+}
+
+function text7()
+{
+    const btn7= document.getElementById('btn7');
+    btn7.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>D1</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+}
+
+function text8()
+{
+    const btn8= document.getElementById('btn8');
+    btn8.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>D2</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text9()
+{
+    const btn9= document.getElementById('btn9');
+    btn9.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>A3</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text10()
+{
+    const btn10= document.getElementById('btn10');
+    btn10.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>A4</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text11()
+{
+    const btn11= document.getElementById('btn11');
+    btn11.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>B3</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text12()
+{
+    const btn12= document.getElementById('btn12');
+    btn12.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>B4</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+  
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+}
+function text13()
+{
+    const btn13= document.getElementById('btn13');
+    btn13.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>C3</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text14()
+{
+    const btn14= document.getElementById('btn14');
+    btn14.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>C4</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text15()
+{
+    const btn15= document.getElementById('btn15');
+    btn15.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>D3</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+function text16()
+{
+    const btn16= document.getElementById('btn16');
+    btn16.style.backgroundColor='greenYellow'
+    const newValue = document.getElementById('set-value');
+    const li = document.createElement('section')
+    li.innerHTML=
+    " <div class='flex gap-16 px-5'><p>D4</p> <p> Economy </p> <p>550</p> ";
+    newValue.appendChild(li);
+
+    const seatLeft = document.getElementById('seat-left')
+    const seatLeftV = parseInt(seatLeft.innerText);
+    console.log(seatLeftV)
+    seatLeft.innerText = seatLeftV - 1;
+
+    const finalValue = document.getElementById('final-man')
+    const finalValueV= parseInt(finalValue.innerText);
+    finalValue.innerText = finalValueV + 1;
+  
+}
+
+
+
+function final ()
+{
+    const mainSection = document.getElementById('main-section')
+    mainSection.classList.add('hidden')
+    const lastSection = document.getElementById('last');
+    lastSection.classList.remove('hidden')
+    
+}
+
+function finallyDone()
+{
+    const  mainSection = document.getElementById('main-section')
+    mainSection.classList.remove('hidden')
+    const lastSection = document.getElementById('last');
+    lastSection.classList.add('hidden')
+}
+
+
+
